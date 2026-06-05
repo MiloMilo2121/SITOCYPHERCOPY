823b579973d8ccc4205954940d48b547b5a80083
MiloMilo2121 <marco.milamelo@gmail.com>
2026-06-05 18:44:20 +0200

    Fix: Use Command S HTML for full DOM render

---
+++ Webild - Idea to website_files/src_app_page_tsx_7f98a96b._.js
@@ -0,0 +1,25 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_e1cd253a._.js",
+  "static/chunks/src_components_sections_11bdb844._.js",
+  "static/chunks/src_components_cardStack_ad62121a._.js",
+  "static/chunks/src_components_bento_262d01e2._.js",
+  "static/chunks/src_components_d09ac4d3._.js",
+  "static/chunks/src_86b73d11._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_56b6fd65._.js",
+  "static/chunks/node_modules_three_build_three_core_996ef05a.js",
+  "static/chunks/node_modules_three_build_three_module_0c59ee63.js",
+  "static/chunks/node_modules_three_build_three_module_f7d46d9b.js",
+  "static/chunks/node_modules_@react-three_fiber_dist_f5d49aef._.js",
+  "static/chunks/node_modules_next_d6119982._.js",
+  "static/chunks/node_modules_recharts_es6_util_3aefc449._.js",
+  "static/chunks/node_modules_recharts_es6_state_a12e5dd0._.js",
+  "static/chunks/node_modules_recharts_es6_component_4d0683f6._.js",
+  "static/chunks/node_modules_recharts_es6_cartesian_6c5cb1bb._.js",
+  "static/chunks/node_modules_recharts_es6_974eb682._.js",
+  "static/chunks/node_modules_e5b94fca._.js",
+  "static/chunks/src_components_e79d92db._.css"
+],
+    source: "dynamic"
+});
---
+++ Webild - Idea to website_files/src_components_background_CanvasRevealBackground_tsx_2d509b28._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_background_021c1871._.js"
+],
+    source: "dynamic"
+});
